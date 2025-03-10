3c5a9ec13b2a9841d00bd1c168c04c6483ad53b0
Miriam2205 <meramlh221@alumnos.cei.es>
2025-03-10 19:00:43 +0100

    ejercicios

--- ejerciciospracticos/ejercicios.js
+++
@@ -1,9 +0,0 @@
-const precio = 200 ;
-        const iva = 21;
-        const precioTotal = precio +precio*21/100
-        console.log("El precio total es: "+precioTotal);
-
-        const prompt = Juan;
-        const nombre = prompt("Juan"); 
-        const saludo = "Hola"+nombre;
-        console.log(saludo);
